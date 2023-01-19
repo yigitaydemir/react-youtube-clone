15ddc6beb2fe634082d96b332eca642a3e4fb734
Yusuf Yigit Aydemir <yusufyigitaydemir@gmail.com>
2023-01-19 03:17:58 +0300

    started

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from "./components";
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar></Navbar>
+      
+      <Routes>
+        <Route path="/" exact element={<Feed></Feed>}></Route>
+        <Route path="/video/:id" element={<VideoDetail></VideoDetail>}></Route>
+        <Route path="/channel/:id" element={<ChannelDetail></ChannelDetail>}></Route>
+        <Route path="/search/:searchTerm" element={<SearchFeed></SearchFeed>}></Route>
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
+
+export default App;
