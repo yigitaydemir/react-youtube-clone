d228561cb62b05eb385b80951be61a5ae0a13076
Yusuf Yigit Aydemir <yusufyigitaydemir@gmail.com>
2023-01-23 17:53:07 +0300

    devam

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Stack, Box } from '@mui/material'
+import { VideoCard, ChannelCard } from "./index"
 
 const Videos = ({ videos }) => {
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
       {videos.map((item, idx) => (
         <Box key={idx}>
-          
+          {item.id.videoId && <VideoCard video={item}></VideoCard>}
+          {item.id.channelId && <ChannelCard channelDetail={item}></ChannelCard>}
         </Box>
       ))}
     </Stack>
