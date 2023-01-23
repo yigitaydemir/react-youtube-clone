1f3df56f7ba720837712714db807186874e8b3da
Yusuf Yigit Aydemir <yusufyigitaydemir@gmail.com>
2023-01-23 18:59:27 +0300

    devam

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants'
 
-const VideoCard = () => {
+const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
   return (
-    <div>VideoCard</div>
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia image={snippet?.thumbnails?.high?.url} alt={snippet?.title} sx={{ width: 358, height: 180 }}></CardMedia>
+      </Link>
+
+      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant='subtitle1' fontWeight="bold" color="#FFF">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
   )
 }
 
